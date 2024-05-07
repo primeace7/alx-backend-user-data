c4d2b9207b48f1ff73e6f04e25a92628c46696f2
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-05-07 16:34:52 +0000

    Add character limits to user db table declaration

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,12 +7,13 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     '''Define user object db structure'''
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=True)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
