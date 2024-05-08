e7d0c266debcfd1e8006183d3c1a661cc8c5afc1
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-05-08 14:59:12 +0000

    Fix uuid token generation for reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,7 +94,7 @@ class Auth:
         '''Generate a token for user to reset password'''
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = uuid4()
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
 
         except NoResultFound:
