56d5820b63176f21cc2bb01f07a2a693d051b4da
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-05-08 14:50:14 +0000

    Fix requirement error in update_password feature

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
         '''Given a user_id, destroy the associated user's session
         '''
         try:
-            user = self._db.find_user_by('user_id'=int(user_id))
+            user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
             return None
 
